8b45d6d1575af602f72b3108486b0d3857a3c708
sallymroberts <sallymroberts@gmail.com>
2016-07-22 16:00:27 -0700

    Tree exercise

---
+++ tree.rb
@@ -0,0 +1,59 @@
+# exercises with trees
+
+# Node class for tree
+class Node
+  attr_accessor :data, :children
+
+  def initialize(data, children)
+    @data = data
+    if children
+      @children = children
+    else
+      @children = []
+    end
+    # @children = []
+  end
+
+  # Add children to node
+  # @param [Array] more_children array of child nodes
+  def add_children(more_children)
+    @children.push(*more_children)
+  end
+
+  # Print node and all its descendants
+  # @param [Node] curr_node
+  def self.print_nodes(curr_node)
+    to_visit = [curr_node]
+
+    while to_visit != []
+      curr_node = to_visit.pop
+      puts curr_node.data
+      to_visit.push(*curr_node.children)
+    end
+  end
+end
+
+# Tree class, composed of node(s)
+class Tree
+  attr_reader :root
+
+  # @param [Node] root node
+  def initialize(root)
+    @root = root
+  end
+end
+
+# Tests of Tree and Node classes
+mary = Node.new("Mary", [])
+dan = Node.new("Dan", [])
+sam = Node.new("Sam", [mary, dan])
+
+Node.print_nodes(sam)
+
+puts
+ralph = Node.new("Ralph", [])
+mary.add_children([ralph])
+Node.print_nodes(sam)
+puts
+sam_family = Tree.new(sam)
+Node.print_nodes(sam_family.root)
