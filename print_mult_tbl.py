59c4914addfb21b852af1f2271c7e4ecbb71ac72
sallymroberts <sallymroberts@gmail.com>
2016-08-30 15:58:25 -0700

    print multiplication tables, exercise 8.19.6 from Think.. scientist Python3"

---
+++ print_mult_tbl.py
@@ -0,0 +1,18 @@
+def print_mult():
+    ''' Print multiplication tables
+    '''
+
+    # Formatted string with layout for each row
+    layout = "{0:>4}{1:>6}{2:>4}{3:>4}{4:>4}{5:>4}{6:>4}{7:>4}{8:>4}{9:>4}{10:>4}{11:>4}{12:>4}"
+    
+    # Print column heading rows
+    print(layout.format("",1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+    under = "   :" + 50 * "-"
+    print(under)
+    
+    # Print multiplication tables detail
+    for i in range(1,13):
+        print(layout.format(str(i) + ":", i, i*2, i*3, i*4, i*5, i*6, i*7, i*8, i*9, i*10, i*11, i*12))
+
+
+print_mult()
