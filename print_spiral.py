c722d72eefe480fe72a44e07d9cdc55bcf85506e
sallymroberts <sallymroberts@gmail.com>
2016-10-25 13:48:25 -0700

    modified comments

--- print_spiral.py
+++ print_spiral.py
@@ -1,118 +1,131 @@
-# Print a 2-dimensional matrix in a spiral, beginning
-# with the top row moving to the right
-
 def spiral_print(matrix):
-	col_max_ind = len(matrix[0]) - 1
-	row_max_ind = len(matrix) - 1
-	row_min_ind = 0
-	col_min_ind = 0
-	num_elem = len(matrix[0]) * len(matrix)
+    """ Print a 2-dimensional matrix in a spiral.
+        Start with top row, moving to the right.
 
-	i = 0 # row index
-	j = 0 # col index
-	direction = "right"
-	count = 0
+        Input: matrix [List] 2-dimensional matrix of positive integers
+        Return: Returns None
 
-	while count < num_elem:
-		# if matrix[i][j] == 11:
-			# print("i: ", end=" ")
-			# print(i, end=" ")
-			# print("j: ", end=" ")
-			# print(j)
+        TEST CASES
+        
+        Square matrix (3 x 3), odd number of rows & columns
+        >>> matrix = [[1, 2, 3],
+        ...           [4, 5, 6],
+        ...           [7, 8, 9],
+        ...           [10,11, 12]]
+        >>> spiral_print(matrix)
+        1 2 3 6 9 12 11 10 7 4 5 8 
 
-		print(matrix[i][j], end=" ")
-		count += 1
-
-		if direction == "right":
-			if j < col_max_ind:
-				j += 1
-			else:
-				row_min_ind += 1
-				direction = "down"
-				i += 1
-		elif direction == "down":
-			if i < row_max_ind:
-				i += 1
-			else:
-				col_max_ind -= 1
-				direction = "left"
-				j -= 1
-		elif direction	== "left":
-			if j > col_min_ind:
-				j -= 1
-			else:
-				row_max_ind -= 1
-				direction = "up"
-				i -= 1
-		elif direction == "up":
-			if i > row_min_ind:
-				i -= 1
-			else:
-				row_min_ind += 1
-				direction = "right"
-				j += 1
-
-matrix = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9],
-          [10,11, 12]]
-
-print("Expect: 1 2 3 6 9 12 11 10 7 4 5 8")
-print("Actual:", end = " ")
-
-spiral_print(matrix)
-print("\n")
-
-matrix = [[1, 2, 3, 4, 5],
-          [6, 7, 8, 9, 10],
-          [11, 12, 13, 14, 15]]
-
-print("Expect: 1 2 3 4 5 10 15 14 13 12 11 6 7 8 9")
-print("Actual:", end = " ")
-spiral_print(matrix)
-print("\n")
+        Square matrix (4 x 4), even number of rows & columns
+        >>> matrix = [[1, 2, 3, 4],
+        ...           [5, 6, 7, 8],
+        ...           [9, 10, 11, 12],
+        ...           [13, 14, 15, 16]]
+        >>> spiral_print(matrix)
+        1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 
 
 
-print("Expect: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10")
-print("Actual:", end = " ")
-matrix = [[1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12],
-          [13, 14, 15, 16]]
+        Not square matrix (5 x 3), odd number of rows & columns
+        >>> matrix = [[1, 2, 3, 4, 5],
+        ...           [6, 7, 8, 9, 10],
+        ...           [11, 12, 13, 14, 15]]
+        >>> spiral_print(matrix)
+        1 2 3 4 5 10 15 14 13 12 11 6 7 8 9 
 
-spiral_print(matrix)
-print("\n")
+        Not square matrix (3 x 4), even number of rows, odd number of columns
+        >>> matrix = [[1, 2, 3],
+        ...           [4, 5, 6],
+        ...           [7, 8, 9],
+        ...           [10,11, 12]]
+        >>> spiral_print(matrix)
+        1 2 3 6 9 12 11 10 7 4 5 8 
 
-print("Expect: 1 2 3 4 8 7 6 5")
-print("Actual:", end = " ")
-matrix = [[1, 2, 3, 4],
-          [5, 6, 7, 8]]
+        Matrix has only 2 rows (2 x 4)
+        >>> matrix = [[1, 2, 3, 4],
+        ...           [5, 6, 7, 8]]
+        >>> spiral_print(matrix)
+        1 2 3 4 8 7 6 5 
 
-spiral_print(matrix)
-print("\n")
+        Matrix has only 1 row (1 x 4)
+        >>> matrix = [[1, 2, 3, 4]]
+        >>> spiral_print(matrix)
+        1 2 3 4 
 
-print("Expect: 1 2 3 4 5")
-print("Actual:", end = " ")
-matrix = [[1, 2, 3, 4, 5]]
+        Matrix has only 1 column (4 x 1)
+        >>> matrix = [[1],
+        ...           [2],
+        ...           [3],
+        ...           [4]]
+        >>> spiral_print(matrix)
+        1 2 3 4 
 
-spiral_print(matrix)
-print("\n")
+        Matrix has 1 row and 1 column (1 x 1)
+        >>> matrix = [[1]]
+        >>> spiral_print(matrix)
+        1 
 
-print("Expect: 1 2 3 4 5")
-print("Actual:", end = " ")
-matrix = [[1], 
-		  [2], 
-		  [3], 
-		  [4], 
-		  [5]]
+        Matrix paramater is invalid: Empty list
+        >>> matrix = []
+        >>> spiral_print(matrix)
+        Matrix must have at least 1 row and 1 column
 
-spiral_print(matrix)
-print("\n")
+        Matrix parameter is invalid: List containing an empty list
+        >>> matrix = [[]]
+        >>> spiral_print(matrix)
+        Matrix must have at least 1 row and 1 column
+    """
+    if len(matrix) < 1:
+        print("Matrix must have at least 1 row and 1 column")
+        return
+    elif len(matrix[0]) < 1:
+        print ("Matrix must have at least 1 row and 1 column")
+        return
 
+    col_max_ind = len(matrix[0]) - 1
+    row_max_ind = len(matrix) - 1
+    row_min_ind = 0
+    col_min_ind = 0
+    num_elem = len(matrix[0]) * len(matrix)
 
+    i = 0 # row index
+    j = 0 # col index
+    direction = "right"
+    count = 0
 
+    while count < num_elem:
 
+        print(matrix[i][j], end=" ")
+        count += 1
 
+        if direction == "right":
+            if j < col_max_ind:
+                j += 1
+            else:
+                row_min_ind += 1
+                direction = "down"
+                i += 1
+        elif direction == "down":
+            if i < row_max_ind:
+                i += 1
+            else:
+                col_max_ind -= 1
+                direction = "left"
+                j -= 1
+        elif direction  == "left":
+            if j > col_min_ind:
+                j -= 1
+            else:
+                row_max_ind -= 1
+                direction = "up"
+                i -= 1
+        elif direction == "up":
+            if i > row_min_ind:
+                i -= 1
+            else:
+                row_min_ind += 1
+                direction = "right"
+                j += 1
 
-
-
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. GREAT!\n")
